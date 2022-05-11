0bc39c01df014fbf707ed805dc5276e63eab5a94
ILDAR1976 <akhmetov.ih@yandex.ru>
2022-05-11 12:50:49 +0300

    Added media type converter to properly display custom content

--- base/src/main/java/ru/base/config/WebConfig.java
+++ base/src/main/java/ru/base/config/WebConfig.java
@@ -2,13 +2,22 @@ package ru.base.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,6 +34,27 @@ public class WebConfig implements WebMvcConfigurer {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         jsonConverter.setObjectMapper(objectMapper);
         return jsonConverter;
+    }
+
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> messageConverters) {
+        messageConverters.add(stringHttpMessageConverter());
+        messageConverters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+    @Bean
+    public StringHttpMessageConverter stringHttpMessageConverter() {
+      Charset UTF8 = Charset.forName("UTF-8");
+      StringHttpMessageConverter bean = new StringHttpMessageConverter(UTF8);
+      List<MediaType> supportedMediaTypes = new ArrayList<>();
+      supportedMediaTypes.add(new MediaType("text", "plain", UTF8));
+      supportedMediaTypes.add(new MediaType("*", "*", UTF8));
+      supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+      supportedMediaTypes.add(MediaType.TEXT_HTML);
+      supportedMediaTypes.add(MediaType.ALL);
+      bean.setSupportedMediaTypes(supportedMediaTypes);
+      return bean;
     }
     
 }
